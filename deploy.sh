beadce6ff3f3c5cc1cdac154681fd92fb2d43d77
joakim brorsson <b.joakim@gmail.com>
2015-09-15 13:04:32 +0200

    m

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ CREATE_DB="create database $DB;"
 USE_DB="use $DB;"
 CREATE_TABLE_PRODUCTS="create table products (product_id int not null auto_increment, title varchar(100) not null, description varchar(1000) not null, img_path varchar(200) not null, primary key (product_id) );"
 
-	QUERY=${CREATE_USER}${CREATE_DB}${USE_DB}${CREATE_TABLE_PRODUCTS}
+QUERY=${CREATE_USER}${CREATE_DB}${USE_DB}${CREATE_TABLE_PRODUCTS}
 
 mysql -u root -p -e "$QUERY"
