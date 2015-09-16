fcd60ee9447f7741ad6f6e0cbe723805a511c381
joakim brorsson <b.joakim@gmail.com>
2015-09-16 09:07:44 +0200

    inventory fills up automatically on deployment

--- deployment/fill_product_inventory.php
+++ deployment/fill_product_inventory.php
@@ -7,6 +7,12 @@ $inventory_file = "./products/inventory.txt";
 $inventory = fopen($inventory_file, "r") or die("Unable to open file!");
 //echo fread($inventory,filesize($inventory_file));
 
+global $argc, $argv;
+$db = $argv[1];
+$user = $argv[2];
+$pass = $argv[3];
+$pdo = new PDO("mysql:host=localhost;dbname=$db", $user, $pass);
+
 while (!feof($inventory)){
    $line = fgets($inventory);
    if(!strcmp($line, "PRODUCT")) break;
@@ -14,16 +20,21 @@ while (!feof($inventory)){
    $price= fgets($inventory);
    $description = fgets($inventory);
    $imgPath = fgets($inventory);
-   insert($name, $price, $description, $imgPath);
+   insert($pdo, $name, $price, $description, $imgPath);
 }
 
 fclose($inventory);
 
-function insert($name, $price, $description, $imgPath){
-	global $argc, $argv;
-	$db = $argv[1];
-	$user = $argv[2];
-	$pass = $argv[3];
+function insert($pdo, $name, $price, $description, $imgPath){
+	
+	$params = array(':title' => $name, ':price' => $price, ':description' => $description, ':img_path' => $imgPath);
+     
+	$stmt = $pdo->prepare("INSERT INTO products (title, price, description, img_path) VALUES (?, ?, ?, ?)");
+	$stmt->bindParam(1, $name);
+	$stmt->bindParam(2, $price);
+	$stmt->bindParam(3, $description);
+	$stmt->bindParam(4, $imgPath);
+	$stmt->execute();
 	
 }
 
