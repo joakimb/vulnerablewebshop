b38172582d747a7e44eb5506feaf7c2832de7578
joakim brorsson <b.joakim@gmail.com>
2015-09-16 16:29:15 +0200

    css

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
 error_reporting(-1);
 ini_set('display_errors', 1);
-
 include 'StoreFront.php';
 ?>
+
 <html>
 <head>
-  <title>phones-R-us</title>
+	<title>phones-R-us</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-<?php
-showStoreFront();
-?>
-
-
+	<div class="main">
+		<?php showStoreFront();?>
+	</div>
 </body>
 </html>
