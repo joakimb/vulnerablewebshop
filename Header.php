e6e741907b1c6eaabef5298680e268d1baf2b348
joakim brorsson <b.joakim@gmail.com>
2015-09-21 11:21:19 +0200

    butons

--- Header.php
+++ Header.php
@@ -3,8 +3,17 @@
 		<h2 id="sf_header">phones-R-us</h2>
 	</div>
 	<div>
-		<button id="logsign" type="button">Login</button>
-		<button id="logsign" type="button">Sign up</button>
+
+		<form id="logsign">
+			<input type="button" onClick="parent.location='header.php'" value="Sign up">
+		</form>
+
+		<form id="logsign" method="link" action="Login.php">
+			<input type="text" name="username" value="username">
+			<input type="text" name="password" value="password">
+			<input type="submit" value="Sign in">
+		</form>
+
 	</div>
 </div>
 
